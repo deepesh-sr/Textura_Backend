d4178a65e889a43a65236d9be3d1662646b7543c
deepesh-sr <220714100078@cutm.ac.in>
2026-02-10 16:30:31 +0530

    Add database connectino setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello Deepeshhhhh")
+import express from "express"
+import mongoose, { mongo } from "mongoose";
+import dotenv from 'dotenv'
+
+dotenv.config();
+const app = express();
+if (process.env.MONGO_URL) {
+    mongoose.connect(process.env.MONGO_URL)
+    console.log("Mongo Db connected")
+} else {
+    console.error("Error connecting database")
+}
+app.get('/', (req, res) => {
+    res.send("Hello")
+})
+app.listen(3000, () => {
+    console.log("Hello, server is running on port 3000")
+})
